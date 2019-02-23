6338d20d590d35cdf98d44f44944fd06902f89a1
Jaeyeon Kim <jaeyeonling@gmail.com>
2019-02-23 16:48:42 +0900

    add parser middleware

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import morgan from 'morgan'
 import helmet from 'helmet'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
 
 const app = express()
 
@@ -18,8 +20,11 @@ const profileHandler = (req, res) => {
   res.send('Hello from profile')
 }
 
-app.use(morgan("dev"))
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(helmet())
+app.use(morgan("dev"))
 
 app.get('/', homeHandler)
 app.get('/profile', profileHandler)
