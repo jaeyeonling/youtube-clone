dce559faae24b9f4ecba5beaacd20b7e48ce044a
Jaeyeon Kim <jaeyeonling@gmail.com>
2019-02-23 16:18:22 +0900

    simple routing

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ const listenHandler = () => {
   console.log(`Listening on: http://localhost:${PORT}`)
 }
 
+const homeHandler = (req, res) => {
+  res.send('Hello from home')
+}
+
+const profileHandler = (req, res) => {
+  res.send('Hello from profile')
+}
+
+app.get('/', homeHandler)
+app.get('/profile', profileHandler)
+
 app.listen(PORT, listenHandler)
