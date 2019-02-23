594ebe39d2042fc74fdb464f1dd9f3e9e9965c18
Jaeyeon Kim <jaeyeonling@gmail.com>
2019-02-23 16:06:06 +0900

    add listen handler and port

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const express = require('express')
 
 const app = express()
 
-app.listen(4000)
+const PORT = 4000
+
+const listenHandler = () => {
+  console.log(`Listening on: http://localhost:${PORT}`)
+}
+
+app.listen(PORT, listenHandler)
