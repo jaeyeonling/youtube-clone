9102bbd73df292dc3dba3514e5fd63de8d0315ae
Jaeyeon Kim <jaeyeonling@gmail.com>
2019-02-23 16:57:21 +0900

    add simple router

--- app.js
+++ app.js
@@ -4,15 +4,9 @@ import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 
+import { userRouter } from './router'
+
 const app = express()
-
-const homeHandler = (req, res) => {
-  res.send('Hello from home')
-}
-
-const profileHandler = (req, res) => {
-  res.send('Hello from profile')
-}
 
 app.use(cookieParser())
 app.use(bodyParser.json())
@@ -20,7 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(helmet())
 app.use(morgan("dev"))
 
-app.get('/', homeHandler)
-app.get('/profile', profileHandler)
+app.use('/user', userRouter)
 
 export default app
