61d5b125a155889460663b37f28d31ac36423050
Jaeyeon Kim <jaeyeonling@gmail.com>
2019-02-24 09:51:27 +0900

    add join page and login page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
-export const join = (req, res) => res.send('Join')
-export const login = (req, res) => res.send('Login')
+export const join = (req, res) => res.render('join', { pageTitle: 'join' })
+export const login = (req, res) => res.render('login', { pageTitle: 'Login' })
 export const logout = (req, res) => res.send('Logout')
 
 export const users = (req, res) => res.send('Logout')
