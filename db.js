2bb9ab30133af1c6bfbded316774e71c7262da7c
Jaeyeon Kim <jaeyeonling@gmail.com>
2019-02-24 10:26:36 +0900

    add fake DB

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 1,
+    title: 'Video awesome',
+    description: 'This is something I love',
+    views: 10,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 1,
+      name: 'Jaeyeon Kim',
+      email: 'jaeyeonling@gmail.com',
+    },
+  },
+  {
+    id: 2,
+    title: 'Video nice',
+    description: 'This is something I love',
+    views: 11,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 1,
+      name: 'Jaeyeon Kim',
+      email: 'jaeyeonling@gmail.com',
+    },
+  },
+  {
+    id: 3,
+    title: 'Video perfact',
+    description: 'This is something I love',
+    views: 1213,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 1,
+      name: 'Jaeyeon Kim',
+      email: 'jaeyeonling@gmail.com',
+    },
+  },
+  {
+    id: 4,
+    title: 'Video very',
+    description: 'This is something I love',
+    views: 124,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      id: 1,
+      name: 'Jaeyeon Kim',
+      email: 'jaeyeonling@gmail.com',
+    },
+  },
+]
