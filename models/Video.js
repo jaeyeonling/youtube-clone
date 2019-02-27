eac54c86f11638d2fb61756154b90b38dcebcdf5
Jaeyeon Kim <jaeyeonling@gmail.com>
2019-02-27 22:43:05 +0900

    video to Video

--- models/Video.js
+++ models/Video.js
@@ -20,6 +20,6 @@ const VideoSchema = new mongoose.Schema({
   },
 })
 
-const model = mongoose.model('video', VideoSchema)
+const model = mongoose.model('Video', VideoSchema)
 
 export default model
