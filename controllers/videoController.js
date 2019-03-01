e5618a6cc81d9b16c740f6ff2303c28c0189b52f
Jaeyeon Kim <jaeyeonling@gmail.com>
2019-03-01 11:05:10 +0900

    home video sorting

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -4,7 +4,9 @@ import Video from '../models/Video'
 export const home = async (req, res) => {
   let videos = []
   try {
-    videos = await Video.find({})
+    videos = await Video.find({}).sort({
+      '_id': -1,
+    })
   } catch (err) {
     console.error(err)
   }
