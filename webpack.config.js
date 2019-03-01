e71b3d3cae44e0df7aee9068f3dcd0ed79145d08
Jaeyeon Kim <jaeyeonling@gmail.com>
2019-03-01 13:25:46 +0900

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,14 @@ const config = {
   module: {
     rules: [
       {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
         test: /\.(scss)$/,
         use: ExtractCSS.extract([
           {
