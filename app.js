8bd07943e495b596d754773f6d60105b3c1bb0e2
Jaeyeon Kim <jaeyeonling@gmail.com>
2019-03-01 13:30:39 +0900

    include static files

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(morgan("dev"))
 app.use(localsMiddleware)
 
 app.use('/uploads', express.static('uploads'))
+app.use('/static', express.static('static'))
 
 app.use(routes.home, globalRouter)
 app.use(routes.users, userRouter)
