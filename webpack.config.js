cedd5b360beb3e748f5aa305a8b044bdaee40a7d
Jaeyeon Kim <jaeyeonling@gmail.com>
2019-03-01 13:22:25 +0900

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const ExtractCSS = require('extract-text-webpack-plugin')
+
+const PROFILE = process.env.PROFILE
+
+const ENTRY_FILE = path.resolve(__dirname, 'assets', 'js', 'main.js')
+const OUTPUT_DIR = path.join(__dirname, 'static')
+
+const config = {
+  entry: ENTRY_FILE,
+  mode: PROFILE,
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: ExtractCSS.extract([
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugin() {
+                return [
+                  autoprefixer({
+                    browsers: 'cover 99.5%',
+                  }),
+                ]
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ]),
+      },
+    ],
+  },
+  output: {
+    path: OUTPUT_DIR,
+    filename: '[name].js',
+  },
+  plugins: [
+    new ExtractCSS('styles.css'),
+  ],
+}
+
+module.exports = config
