a3ac9355529cd1f71a5918e3f1bcf273cb3336de
Jaeyeon Kim <jaeyeonling@gmail.com>
2019-03-01 14:26:10 +0900

    styling footer

--- middlewares.js
+++ middlewares.js
@@ -11,7 +11,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.routes = routes
 
   res.locals.user = {
-    isAuthenticated: true,
+    isAuthenticated: false,
     id: 1,
   }
 
