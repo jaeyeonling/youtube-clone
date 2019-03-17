f0da1dfe497ace26028387fe001778d45d87b607
Jaeyeonling <jaeyeonling@gmail.com>
2019-03-17 23:12:29 +0900

    add videoplayer fullscreen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,10 +2,14 @@ const videoContainer = document.getElementById('videoPlayer')
 const videoPlayer = document.querySelector('#videoPlayer video')
 const playButton = document.getElementById('playButton')
 const volumeButton = document.getElementById('volumeButton')
+const fullScreenButton = document.getElementById('fullScreenButton')
+
+let isFullScreen = false
 
 function init() {
     playButton.addEventListener('click', playHandler)
     volumeButton.addEventListener('click', volumeHandler)
+    fullScreenButton.addEventListener('click', fullScreenHandler)
 }
 
 function playHandler() {
@@ -19,13 +23,25 @@ function playHandler() {
 }
 
 function volumeHandler() {
-    if(videoPlayer.muted) {
+    if (videoPlayer.muted) {
         volumeButton.innerHTML = '<i class="fas fa-volume-up"></i>'
     } else {
         volumeButton.innerHTML = '<i class="fas fa-volume-mute"></i>'
     }
 
     videoPlayer.muted = !videoPlayer.muted
+}
+
+function fullScreenHandler() {
+    if (isFullScreen) {
+        document.webkitExitFullscreen()
+        fullScreenButton.innerHTML = '<i class="fas fa-expand"></i>'
+    } else {
+        videoContainer.webkitRequestFullscreen()
+        fullScreenButton.innerHTML = '<i class="fas fa-compress"></i>'
+    }
+
+    isFullScreen = !isFullScreen
 }
 
 if (videoContainer) {
