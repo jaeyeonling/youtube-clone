11244e0576681500bcc583ab111533fd39d16434
Jaeyeonling <jaeyeonling@gmail.com>
2019-03-18 22:20:33 +0900

    video record and download

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -3,7 +3,40 @@ const recorderContainer = document.getElementById("recordContainer")
 const recordButton = document.getElementById("recordButton")
 const videoPreview = document.getElementById("videoPreview")
 
-async function startRecording() {
+let videoRecorder
+
+function videoDataHandler(event) {
+    const {
+        data: videoFile,
+    } = event
+
+    const link = document.createElement('a')
+    link.href = URL.createObjectURL(videoFile)
+    link.download = 'recorded.webm'
+
+    document.body.appendChild(link)
+
+    link.click()
+}
+
+function stopRecording() {
+    videoRecorder.stop()
+
+    recordButton.removeEventListener('click', stopRecording)
+    recordButton.addEventListener('click', getVideo)
+    recordButton.innerHTML = 'Start recording'
+}
+
+function startRecording() {
+    videoRecorder = new MediaRecorder(videoPreview.srcObject)
+    
+    videoRecorder.start()
+    videoRecorder.addEventListener('dataavailable', videoDataHandler)
+    
+    recordButton.addEventListener('click', stopRecording)
+}
+
+const getVideo = async () => {
     try {
         videoPreview.srcObject = await navigator.mediaDevices.getUserMedia({
             audio: true,
@@ -14,14 +47,18 @@ async function startRecording() {
         })
         videoPreview.muted = true
         videoPreview.play()
+
+        recordButton.innerHTML = 'Stop recording'
+        startRecording()
     } catch (err) {
         recordButton.innerHTML = 'Can not record'
-        recordButton.removeEventListener('click', startRecording)
     }
+
+    recordButton.removeEventListener('click', getVideo)
 }
 
 function init() {
-    recordButton.addEventListener('click', startRecording)
+    recordButton.addEventListener('click', getVideo)
 }
 
 if (recorderContainer) {
